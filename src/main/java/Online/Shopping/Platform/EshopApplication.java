ff5c406734c807ec387c07c8dc2e46df125ff422
van11497 <vanmathi114@gmail.com>
2024-10-24 08:22:04 +0530

    version1

---
+++ src/main/java/Online/Shopping/Platform/EshopApplication.java
@@ -0,0 +1,13 @@
+package Online.Shopping.Platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Online/Shopping/Platform/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package Online.Shopping.Platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
